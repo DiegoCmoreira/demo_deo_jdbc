e8c333caf8f18ea10204bf0356b5589ca247feea
DiegoCmoreira <diego.moreira@sptech.school>
2023-04-15 12:40:32 -0300

    Classe Department

---
+++ src/aplicacao/Program.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import model.entidade.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+	}
+}
